60fc4acdb394f9c659a40f7265e83f7991bf5d44
Артем Горбачев <meltaus@yandex.ru>
2018-07-08 19:12:53 +0700

    Интерфейс описывающий методы установки веса одного фрукта и получени веса всех фруктов

--- lesson1/src/main/java/ru/gorbachev/lesson1/fruit/BaseWorkFruit.java
+++ lesson1/src/main/java/ru/gorbachev/lesson1/fruit/BaseWorkFruit.java
@@ -1,4 +1,11 @@
 package ru.gorbachev.lesson1.fruit;
 
-public interface BaseWorkFruit {
+import java.util.List;
+
+public interface BaseWorkFruit<T extends Fruit> {
+
+    //Установка веса фрукта
+    void setWeight(float weight);
+    //Получить вес всех фруктов
+    void getWeight(List<T> fruitOnBox, float weight);
 }

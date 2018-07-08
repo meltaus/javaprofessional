088b7412a26f9c72968c5e60c020f834121414d8
Артем Горбачев <meltaus@yandex.ru>
2018-07-08 18:59:36 +0700

    Смена мест указанных элементов массива ссылочного типа

--- lesson1/src/main/java/ru/gorbachev/lesson1/workarray/ChangeElements.java
+++ lesson1/src/main/java/ru/gorbachev/lesson1/workarray/ChangeElements.java
@@ -1,4 +1,33 @@
 package ru.gorbachev.lesson1.workarray;
 
-public class ChangeElements {
+import java.io.Serializable;
+
+public class ChangeElements<T extends Serializable> {
+
+    int element1;
+    int element2;
+    T[] arr;
+
+    //В конструктор передаются ндексы элементо, которые неободимо поменять
+    public ChangeElements(T[] arr, int element1, int element2) {
+        this.arr = arr;
+        int count = arr.length;
+        if (element1 > count) {
+            element1 = count;
+            System.out.print("Индекс 1 выходит за переделы массива. Присвоено значение крайнего элемента");
+        }
+        if (element2 > count) {
+            element2 = count;
+            System.out.print("Индекс 2 выходит за переделы массива. Присвоено значение крайнего элемента");
+        }
+        this.element1 = element1;
+        this.element2 = element2;
+    }
+
+    public T[] change(T[] arr) {
+        T tmpElement = arr[element1];
+        arr[element1]= arr[element2];
+        arr[element2] = tmpElement;
+        return arr;
+    }
 }

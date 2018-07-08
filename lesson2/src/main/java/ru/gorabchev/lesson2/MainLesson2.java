2c6269b78936dd8310b3fc64bc0e31c71871cf0c
Артем Горбачев <meltaus@yandex.ru>
2018-07-08 21:55:04 +0700

    Деонстрационный код

--- lesson2/src/main/java/ru/gorabchev/lesson2/MainLesson2.java
+++ lesson2/src/main/java/ru/gorabchev/lesson2/MainLesson2.java
@@ -4,8 +4,7 @@ import ru.gorabchev.lesson2.essence.Product;
 import ru.gorabchev.lesson2.sqlitedb.DbHandler;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public class MainLesson2 {
@@ -23,6 +22,74 @@ public class MainLesson2 {
             e.printStackTrace();
         }
 
+        createProduct();
+
+        consoleMenu();
+    }
+
+    //Консольное меню приложения
+    private static void consoleMenu() {
+        int numberMenu = -1;
+        while (numberMenu != 0) {
+            System.out.println("Введите комманду: ");
+            Scanner in = new Scanner(System.in);
+            String str = in.nextLine();
+            if (str.contains("/Выход")) {
+                numberMenu = 0;
+            } else if (str.contains("/цена")) {
+                numberMenu = 1;
+            } else if (str.contains("/сменитьцену")) {
+                numberMenu = 2;
+            } else if (str.contains("/товарыпоцене ")) {
+                numberMenu = 3;
+            }
+
+            switch (numberMenu) {
+                case 1:
+                    str = str.replace("/цена ","");
+                    if (!"".equals(str)) {
+                        Map<String, String> where = new HashMap<>();
+                        where.put("title", str);
+                        List<Product> productList = dbHandler.selectWithWhere(where, "and", "=");
+                        System.out.println("Цена запрошенного товара: " + productList.get(0).getCost());
+                    }
+                    break;
+
+                case 2:
+                    str = str.replace("/сменитьцену ","");
+                    if (!"".equals(str)) {
+                        Map<String, String> where = new HashMap<>();
+                        List<Product> productList;
+                        String[] elements = str.split(" ");
+                        where.put("title", elements[0]);
+                        System.out.print("Старое значение:\t");
+                        productList = dbHandler.selectWithWhere(where, "and", "=");
+                        System.out.println(productList.get(0).toString());
+                        dbHandler.updateWithWhere("cost", where, "and", elements[1]);
+                        System.out.print("Новое значение:\t");
+                        productList = dbHandler.selectWithWhere(where, "and", "=");
+                        System.out.println(productList.get(0).toString());
+                    }
+                    break;
+
+                case 3:
+                    str = str.replace("/товарыпоцене ","");
+                    if (!"".equals(str)) {
+                        Map<String, String> where = new HashMap<>();
+                        where.put("cost", str);
+                        List<Product> productList = dbHandler.selectWithWhere(where, "and", "=");
+                        System.out.println("Товары, которые соответствуют указанной цене: ");
+                        for (Product product : productList) {
+                            System.out.println(product.toString());
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    //Создаем в БД требуемое количество записей
+    private static void createProduct() {
         Thread genProdId1 = new Thread(() -> generateProdId());
         Thread genProdId2 = new Thread(() -> generateProdId());
         genProdId1.start();

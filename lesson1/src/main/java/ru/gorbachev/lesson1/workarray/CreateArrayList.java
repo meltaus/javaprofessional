f57eac4c3d991fbe55ae4e4c3748465792a594d0
Артем Горбачев <meltaus@yandex.ru>
2018-07-08 19:02:51 +0700

    Преобразование массива в ArrayList

--- lesson1/src/main/java/ru/gorbachev/lesson1/workarray/CreateArrayList.java
+++ lesson1/src/main/java/ru/gorbachev/lesson1/workarray/CreateArrayList.java
@@ -1,4 +1,19 @@
 package ru.gorbachev.lesson1.workarray;
 
-public class CreateArrayList {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateArrayList<T extends Serializable> {
+
+    List<T> resultList;
+
+    public CreateArrayList(T[] arr) {
+        this.resultList = new ArrayList<T>(Arrays.asList(arr));
+    }
+
+    public List<T> getResultList() {
+        return resultList;
+    }
 }

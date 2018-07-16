4f6daf7d6e1e21afe208f902053a099ee41e0e4e
meltaus <meltaus@yandex.ru>
2018-07-16 11:27:37 +0700

    Отладочный код

--- lesson3/src/main/java/ru/gorbachev/lesson3/MainLesson3.java
+++ lesson3/src/main/java/ru/gorbachev/lesson3/MainLesson3.java
@@ -7,5 +7,6 @@ public class MainLesson3 {
     public static void main(String[] args) {
         ReadFile readFile = new ReadFile();
 //        readFile.readByteFile(50);
+//        readFile.concatinationFile(100, "result.txt");
     }
 }

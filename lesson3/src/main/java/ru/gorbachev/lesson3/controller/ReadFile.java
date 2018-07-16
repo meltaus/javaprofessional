4bc593fdf85a22dbd412875bc9d601861a613323
meltaus <meltaus@yandex.ru>
2018-07-16 11:26:57 +0700

    Склейка переданных файлов

--- lesson3/src/main/java/ru/gorbachev/lesson3/controller/ReadFile.java
+++ lesson3/src/main/java/ru/gorbachev/lesson3/controller/ReadFile.java
@@ -1,8 +1,7 @@
 package ru.gorbachev.lesson3.controller;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Collections;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReadFile {
@@ -38,10 +37,40 @@ public class ReadFile {
         }
     }
 
-    //Читаем указанный файл на требуемый размер и возвращаем список прочитанных строк,
+    //Читаем указанный файл на требуемый размер и пишем в один файл последовательно
     //Предполагает, что файл имеет кодировку utf-8
-    private List<String> readFile (final int leight, final String pathfile) {
+    public byte[] concatinationFile(final int leight, final String pathFile) {
+        CreateRandomFile createRandomFile = new CreateRandomFile();
+        final List<String> filePathList = createRandomFile.createFile(5, leight);
+        List<String> lines = new ArrayList<String>();
 
-        return Collections.emptyList();
+        File file = new File(pathFile);
+        if (file.exists()) {
+            file.delete();
+        }
+
+        try {
+            for (String path : filePathList) {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                String line = reader.readLine();
+
+                while (line != null) {
+                    lines.add(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+            }
+            BufferedWriter writer = new BufferedWriter(new FileWriter(pathFile, true));
+            for (int i = 0; i < lines.size(); i++) {
+                String lineSeparator = System.getProperty("line.separator");
+                writer.write(lines.get(i) + lineSeparator);
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }

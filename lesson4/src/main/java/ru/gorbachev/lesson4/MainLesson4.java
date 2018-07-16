5de3eab5fc2c181e39461d4bfab6065862e8a26d
meltaus <meltaus@yandex.ru>
2018-07-16 17:03:26 +0700

    хз

--- lesson4/src/main/java/ru/gorbachev/lesson4/MainLesson4.java
+++ lesson4/src/main/java/ru/gorbachev/lesson4/MainLesson4.java
@@ -1,7 +1,32 @@
 package ru.gorbachev.lesson4;
 
+import ru.gorbachev.lesson4.controller.PrintChar;
+
 public class MainLesson4 {
     public static void main(String[] args) {
-        
+        final Object msg = new Object();
+        PrintChar printChar = new PrintChar(msg);
+        printChar.start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (msg) {
+                    for (int i = 0; i < 4; i++) {
+                        if (printChar.isAlive());
+                        try {
+                            while (printChar.isLocked()) {
+                                Thread.sleep(500);
+                            }
+                            msg.notify();
+                            System.out.print(i);
+                            printChar.setLocked(true);
+                            System.out.print(printChar.isLocked());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }).start();
     }
 }
--- lesson4/src/main/java/ru/gorbachev/lesson4/controller/PrintChar.java
+++ lesson4/src/main/java/ru/gorbachev/lesson4/controller/PrintChar.java
@@ -1,4 +1,52 @@
 package ru.gorbachev.lesson4.controller;
 
-public class PrintChar {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintChar extends Thread{
+
+    private final Object msg; //Монитор
+    private final List<String> printChar;
+    private int iteration;
+    private boolean locked;
+
+    public PrintChar(Object msg) {
+        this.msg = msg;
+        this.printChar = new ArrayList<>();
+        iteration = 0;
+        printChar.add("A");
+        printChar.add("B");
+        printChar.add("C");
+        locked = true;
+    }
+
+    public void continuePrint() {
+        msg.notify();
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void run() {
+        synchronized (msg) {
+            for (int i = 0; i < 15; i++) {
+                    if (this.iteration == 3) {
+                        this.iteration = 0;
+                        try {
+                            locked = false;
+                            msg.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.print(printChar.get(this.iteration));
+                    this.iteration++;
+            }
+        }
+    }
 }

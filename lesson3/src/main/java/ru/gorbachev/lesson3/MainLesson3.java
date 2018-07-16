1b41ecbf5af64e8a3bb4663411fdac2c17976598
meltaus <meltaus@yandex.ru>
2018-07-16 10:16:35 +0700

    Отладочный код

--- lesson3/src/main/java/ru/gorbachev/lesson3/MainLesson3.java
+++ lesson3/src/main/java/ru/gorbachev/lesson3/MainLesson3.java
@@ -1,8 +1,11 @@
 package ru.gorbachev.lesson3;
 
+import ru.gorbachev.lesson3.controller.ReadFile;
+
 public class MainLesson3 {
 
     public static void main(String[] args) {
-        
+        ReadFile readFile = new ReadFile();
+//        readFile.readByteFile(50);
     }
 }

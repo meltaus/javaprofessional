357f3eebe96398bdf31374282c8734a66e4b2842
Артем Горбачев <meltaus@yandex.ru>
2018-07-22 18:57:27 +0700

    Приведение начального кода к виду требуемому по заданию

--- lesson5/src/main/java/ru/gorbachev/javaprofessiona/lesson5/essence/Race.java
+++ lesson5/src/main/java/ru/gorbachev/javaprofessiona/lesson5/essence/Race.java
@@ -1,4 +1,12 @@
 package ru.gorbachev.javaprofessiona.lesson5.essence;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
 }
--- lesson5/src/main/java/ru/gorbachev/javaprofessiona/lesson5/essence/Stage.java
+++ lesson5/src/main/java/ru/gorbachev/javaprofessiona/lesson5/essence/Stage.java
@@ -1,4 +1,10 @@
 package ru.gorbachev.javaprofessiona.lesson5.essence;
 
-public class Stage {
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
 }
